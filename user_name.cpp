91ac93c1dea504a9538b1862dd0da6cc8e269e3d
rouchi-11 <141231824+rouchi-11@users.noreply.github.com>
2024-03-28 08:11:53 +0530

    user_name.cpp

---
+++ user_name.cpp
@@ -0,0 +1,17 @@
+ 
+
+#include <iostream> 
+using namespace std; 
+
+// Driver code 
+int main() 
+{ 
+string name; 
+	
+cout << "Enter the name: "; 
+cin >> name; 
+	
+cout << "Entered name is: " << 
+		name; 
+	return 0; 
+}

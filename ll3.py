9a1fd9ec5cd76a606e86bfb498c3bbaf97699eaa
rouchi-11 <141231824+rouchi-11@users.noreply.github.com>
2024-08-30 00:38:14 +0530

    ll3.py

--- ll3.py
+++ ll3.py
@@ -1,17 +1,19 @@
-#printing linked list
 class Node:
-    def __init__(self,data,Head):
-        self.data=data
-        self.next=None
-        Head=N1
-        current=Head
-N1=Node(1)
-N2=Node(2)
-N3=Node(3)    
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-N1.next=N2
-N2.next=N3
+N1 = Node(1)
+N2 = Node(2)
+N3 = Node(3)    
+
+
+N1.next = N2
+N2.next = N3
+
+Head = N1
+current = Head
 
 while current is not None:
-    print(current.data,end="-")
-    current=current.next
+    print(current.data, end="-")
+    current = current.next

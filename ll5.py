eb6c1cea641878e4ae15b31dd9bbefc9c222638d
rouchi-11 <141231824+rouchi-11@users.noreply.github.com>
2024-08-30 07:57:55 +0530

    ll5.py

--- ll5.py
+++ ll5.py
@@ -1,19 +1,23 @@
 #to add at beginning
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-       
-        
-N1=Node(1)
-N2=Node(2)
-N3=Node(3)   
-N4=Node(4) 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-N1.next=N2
-N2.next=N3
-N4.next=N4
+N1 = Node(1)
+N2 = Node(2)
+N3 = Node(3)   
+N4 = Node(4) 
 
+N1.next = N2
+N2.next = N3
+
+
+N4.next = N1  
+Head = N4 
+
+
+current = Head
 while current is not None:
-    print(current.data,end="-")
-    current=current.next
+    print(current.data, end="-")
+    current = current.next

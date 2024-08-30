d4eff88da762de2da3b06881ded9aa56cf6747e8
rouchi-11 <141231824+rouchi-11@users.noreply.github.com>
2024-08-30 07:56:36 +0530

    ll4.py

--- ll4.py
+++ ll4.py
@@ -1,30 +1,28 @@
-#to insert at end
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-        
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+N1 = Node(1)
+N2 = Node(2)
+N3 = Node(3)
 
-N1=Node(1)
-N2=Node(2)
-N3=Node(3)
-    
+N1.next = N2
+N2.next = N3
 
-N1.next=N2
-N2.next=N3
-
-Head=N1
-current=Head
-N4=Node(4)
-
+Head = N1
+current = Head
+N4 = Node(4)
 
 
 while current.next is not None:
-    current=current.next
-current.next=N4
-    
-current=Head
+    current = current.next
+
+
+current.next = N4
+
+
+current = Head
 while current is not None:
-    print(current.data,end="-")
-    current=current.next
+    print(current.data, end="-")
+    current = current.next
